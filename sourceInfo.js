62c04bd33d8914dd83ed311b4423b99545e4cd19
Lenard Frommelt <lenny09918050@googlemail.com>
2021-04-18 13:07:03 +0200

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -3,8 +3,8 @@ module.exports = {
         return {
             "mustache": "^4.2.0",
             "qr-scanner": "^1.2.0",
-            "secret-manager-client": "0.0.1",
-            "secret-manager-crypto-utils": "0.0.5",
+            "secret-manager-client": "0.0.2",
+            "secret-manager-crypto-utils": "0.0.6",
             "vanillaqr": "^1.0.0"        
         }
     }
